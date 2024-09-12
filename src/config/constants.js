6188b26253246215ecf8f3ad081d97d788cac098
AdamAFD <ahmed.adam.2078@gmail.com>
2024-09-12 20:33:13 +0300

    test env

--- src/config/constants.js
+++ src/config/constants.js
@@ -9,7 +9,7 @@ const ConfigurableValues = () => {
   // const WS_SERVER_URL = 'ws://192.168.0.107:8001'
   //.envvvvvv
   const GRAPHQL_URL='https://api-heart-attack-prod-277226964694.europe-west3.run.app'
-  const SERVER_URL = 'https://admin.heartattack.shop'
+  const SERVER_URL = 'https://api-heart-attack-prod-277226964694.europe-west3.run.app'
   const WS_SERVER_URL = 'wss://api-heart-attack-prod-277226964694.europe-west3.run.app'
   const GOOGLE_MAPS_KEY = 'AIzaSyCrSZxTEqjc5qnyg4EghQVJz2I6KuEcuwg'
   const FIREBASE_KEY = configuration.firebaseKey
