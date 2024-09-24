ec3a12dc1d5240eea9c500b7e69e05b32b9d0fe2
ibrahimasry <imasry@intouch.com>
2024-09-24 19:28:20 +0300

    add order table for admin and for super admin && disable husky

--- src/views/Orders.jsx
+++ src/views/Orders.jsx
@@ -1,44 +1,55 @@
-import React, { useState } from 'react'
-import { withTranslation } from 'react-i18next'
-import OrderComponent from '../components/Order/Order'
-import OrdersData from '../components/Order/OrdersData'
-import Header from '../components/Headers/Header'
-import { useQuery, gql } from '@apollo/client'
-import { getOrdersByRestaurant } from '../apollo'
-import useGlobalStyles from '../utils/globalStyles'
-import { Container, Modal } from '@mui/material'
+import React, { useState } from "react";
+import { withTranslation } from "react-i18next";
+import { useLocation } from "react-router-dom";
+
+import OrderComponent from "../components/Order/Order";
+import OrdersData from "../components/Order/OrdersData";
+import Header from "../components/Headers/Header";
+import { useQuery, gql } from "@apollo/client";
+import { getOrders, getOrdersByRestaurant } from "../apollo";
+import useGlobalStyles from "../utils/globalStyles";
+import { Container, Modal } from "@mui/material";
 
 const GET_ORDERS = gql`
   ${getOrdersByRestaurant}
-`
+`;
+const GET_ALL_ORDERS = gql`
+  ${getOrders}
+`;
 
 const Orders = () => {
-  const [detailsModal, setDetailModal] = useState(false)
-  const [order, setOrder] = useState(null)
-  const [page, setPage] = useState(1)
-  const [rowsPerPage, setRowsPerPage] = useState(10)
-  const [search] = useState('')
-  const restaurantId = localStorage.getItem('restaurantId')
+  const [detailsModal, setDetailModal] = useState(false);
+  const [order, setOrder] = useState(null);
+  const [page, setPage] = useState(1);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [search] = useState("");
+  const restaurantId = localStorage.getItem("restaurantId");
+  const location = useLocation();
 
+  // Determine the value of isAll based on the current URL
+  const isAll = location.pathname.includes("/all-orders");
+
+  // Log to see if isAll is set correctly
+  console.log(isAll);
   const {
     data,
     error: errorQuery,
     loading: loadingQuery,
-    subscribeToMore
-  } = useQuery(GET_ORDERS, {
+    subscribeToMore,
+  } = useQuery(isAll ? GET_ALL_ORDERS : GET_ORDERS, {
     variables: {
-      restaurant: restaurantId,
+      restaurant: isAll ? undefined : restaurantId,
       page: page - 1,
       rows: rowsPerPage,
-      search
-    }
-  })
-  const toggleModal = order => {
-    setOrder(order)
-    setDetailModal(!detailsModal)
-  }
+      search: isAll ? undefined : search,
+    },
+  });
+  const toggleModal = (order) => {
+    setOrder(order);
+    setDetailModal(!detailsModal);
+  };
 
-  const globalClasses = useGlobalStyles()
+  const globalClasses = useGlobalStyles();
   return (
     <>
       <Header />
@@ -47,11 +58,11 @@ const Orders = () => {
       <Container className={globalClasses.flex} fluid>
         {errorQuery && (
           <tr>
-            <td>{`${'Error'} ${errorQuery.message}`}</td>
+            <td>{`${"Error"} ${errorQuery.message}`}</td>
           </tr>
         )}
         <OrdersData
-          orders={data && data.ordersByRestId}
+          orders={data && (data.ordersByRestId || data.allOrders)}
           toggleModal={toggleModal}
           subscribeToMore={subscribeToMore}
           loading={loadingQuery}
@@ -62,18 +73,19 @@ const Orders = () => {
         />
         <Modal
           style={{
-            width: '75%',
-            marginLeft: '13%',
-            overflowY: 'auto'
+            width: "75%",
+            marginLeft: "13%",
+            overflowY: "auto",
           }}
           open={detailsModal}
           onClose={() => {
-            toggleModal(null)
-          }}>
+            toggleModal(null);
+          }}
+        >
           <OrderComponent order={order} />
         </Modal>
       </Container>
     </>
-  )
-}
-export default withTranslation()(Orders)
+  );
+};
+export default withTranslation()(Orders);
